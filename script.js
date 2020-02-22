a68f87d047358b4034a77a133b6ec4ca27fb7eee
Michelle Trinh <55078206+AmazedCheesequeen@users.noreply.github.com>
2020-02-23 00:14:12 +0100

    indsat pop op der virker

--- script.js
+++ script.js
@@ -45,15 +45,54 @@ function visKager() {
         if (kage.gsx$kategori.$t == filter || filter == "alle") {
             let klon = template.cloneNode(true).content;
             klon.querySelector("h2").textContent = kage.gsx$kategori.$t;
-            klon.querySelector("img").src = `../img/${kage.gsx$billede.$t}.jpg`;
+            klon.querySelector(".billede").src = `../img/${kage.gsx$billede.$t}.jpg`;
             klon.querySelector(".navn").textContent += kage.gsx$navn.$t;
-            klon.querySelector(".beskrivelse").textContent += kage.gsx$beskrivelse.$t;
+            //klon.querySelector(".beskrivelse").textContent += kage.gsx$beskrivelse.$t;
 
-            klon.querySelector(".kage").addEventListener("click", () => {
-                location.href = `popup.html?id=${kage.gsx$id.$t}`;
-            });
+            //Bruges til at vide hvornår pop up skal sættes op (jeg skal lige spørge en ven om alt det tekniske bag det så kommer jeg med en bedre forklaring)
+
+            let event = klon.querySelector(".billede");
+
+            event.addEventListener("click", popOp, false);
+
+            event.kage = kage;
 
             container.appendChild(klon);
         }
     });
+}
+
+function popOp (event) {
+
+    let popOpScreen = document.querySelector(".pop_op");
+
+    //Hent information fra json fil for specifik kage man har trykket på
+    let jsonPop = event.currentTarget.kage;
+
+    //Insæt billede med alt og src
+
+    popOpScreen.querySelector(".pop_billede").src = "../img/" + jsonPop.gsx$billede.$t + ".jpg";
+
+    popOpScreen.querySelector(".pop_billede").alt = jsonPop.gsx$navn.$t;
+
+    //Indsæt beskrivelsen
+    popOpScreen.querySelector(".beskrivelse").textContent = jsonPop.gsx$beskrivelse.$t;
+
+    //Display popOp
+
+    document.querySelector(".pop_op").style.display = "block";
+
+    //Tilføj eventlistener så man kan trykke væk fra pop op igen
+
+    document.querySelector(".exit").addEventListener("click", popGone)
+
+}
+
+function popGone() {
+
+    //Fjern pop op skærm og eventlistener
+
+    document.querySelector(".pop_op").style.display = "none";
+    document.querySelector(".exit").removeEventListener("click", popGone)
+
 }
