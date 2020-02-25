70ca263d12e9b8f23c1a719090ce31c59a51b469
Zainab Waleed <ZainabW@Air.local>
2020-02-25 12:17:36 +0100

    json

--- credit-script.js
+++ credit-script.js
@@ -3,3 +3,11 @@ document.addEventListener("DOMContentLoaded", start);
 
 const idSheet = "13ahAGLQCN5piGwRaCvH8r-V6Xm5LgIraxCeZsWFMIE4";
 const endpoint = `https://spreadsheets.google.com/feeds/list/${idSheet}/od6/public/values?alt=json`;
+
+
+async function loadData() {
+    const response = await fetch(endpoint);
+    credit = await response.json();
+    console.log(credit);
+    visCredit();
+}
