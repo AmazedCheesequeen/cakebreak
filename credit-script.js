016fcb7c6479b6da16f1a70f67cca79ae58d9759
Zainab Waleed <ZainabW@Air.local>
2020-02-25 12:10:10 +0100

    json credits

---
+++ credit-script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", start);
+
+
+const idSheet = "13ahAGLQCN5piGwRaCvH8r-V6Xm5LgIraxCeZsWFMIE4";
+const endpoint = `https://spreadsheets.google.com/feeds/list/${idSheet}/od6/public/values?alt=json`;
